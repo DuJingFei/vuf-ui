c031b258279d265b45e95b5c5c413c6a72e4ed89
jingfei_du <jingfei_du@kingdee.com>
2019-11-06 11:23:59 +0800

    树形组件初始化

--- src/components/tree/src/tree.vue
+++ src/components/tree/src/tree.vue
@@ -1,36 +1,51 @@
 <template>
-   <div class="kd-tree">
-      <section class="kd-product-line">
+   <div class="kd-tree card root">
+      <section class="kd-product-line" v-for="(productLine , pl) in testData">
         <p class="kd-product-bar">
-          <span class="kd-product-name">财务产品</span>
+          <span class="kd-product-name">{{productLine.name}}</span>
           <span>V</span>
         </p>
-        <ul class="kd-rank-domain">
-           <p class="kd-doamin-bar">
-            <span class="kd-doamin-name">财务领域</span>
-            <span>V</span>
-           </p>
-           <ul class="kd-rank-module">
-              <!-- 业务对象 -->
-              <p class="kd-module-bar">
-                 <span class="kd-module-name">财务模块</span>
-                 <span>V</span>
+        <ul class="kd-rank-product" v-if="productLine.childs && productLine.childs.length > 0">
+           <section class="kd-product" v-for="(product , p) in productLine.childs">
+              <p class="kd-product-bar">
+                <span class="kd-product-name">{{product.name}}</span>
+                <span>V</span>
               </p>
-              <ul class="kd-rank-bizentity">
-                 <p class="kd-bizentity-bar">
-                    <span class="kd-bizentity-name">财务对象</span>
-                    <span>V</span>
-                 </p>
+              <ul class="kd-rank-domain" v-if="product.childs && product.childs.length > 0">
+                <section class="kd-domain" v-for="(domain , d) in product.childs">
+                        <p class="kd-doamin-bar">
+                            <span class="kd-doamin-name">{{domain.name}}</span>
+                            <span>V</span>
+                        </p>
+                        <ul class="kd-rank-module" v-if="domain.childs && domain.childs.length > 0">
+                            <section v-for="(moduleObj, d) in domain.childs">
+                               <p class="kd-module-bar">
+                                  <span class="kd-module-name">{{moduleObj.name}}</span>
+                                  <span>V</span>
+                                </p>
+                                <ul class="kd-rank-bizentity" v-if="moduleObj.childs && moduleObj.childs.length > 0">
+                                    <p class="kd-bizentity-bar" v-for="(bizentity, b) in moduleObj.childs">
+                                        <span class="kd-bizentity-name">{{bizentity.name}}</span>
+                                        <span>V</span>
+                                    </p>
+                                </ul>
+                            </section>
+                        </ul>
+                </section>
               </ul>
-           </ul>
+           </section>
         </ul>
       </section>
    </div>
 </template>
 <script>
 export default {
-   name: 'kd-tree',
-
+  name: 'kd-tree',
+  data() {
+      return {
+          testData: require('../treeData.json')
+      }
+  },
 }
 </script>
 <style lang="less">
